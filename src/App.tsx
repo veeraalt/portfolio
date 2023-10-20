64ab8568ef615fc4ef4477b28501e25b560fc81d
Veera Alt <veera.alt@outlook.com>
2023-10-20 11:35:26 +0300

    Add routing to landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LandingPage from "./components/landingPage/LandingPage";
 
 function App() {
-  return <LandingPage />;
+  return (
+    <BrowserRouter basename="/portfolio">
+      <Routes>
+        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
