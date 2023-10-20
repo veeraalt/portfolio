c677906b556beeeb009079c868492c48b8bd1bc6
Veera Alt <veera.alt@outlook.com>
2023-10-20 17:18:28 +0300

    Add mobile styling

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,17 +1,31 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { FaBars as MenuIcon } from "react-icons/fa6";
 import "./Navbar.css";
 
 export function Navbar() {
+  const [menuExpanded, setMenuExpanded] = useState(false);
   return (
     <nav className="navbar">
       <NavLink className="navlink" to="/">
         Home
       </NavLink>
-      <NavLink className="navlink" to="/projects">
-        Projects
-      </NavLink>
-      <NavLink to="/cv">CV</NavLink>
-      <NavLink to="/contact">Contact</NavLink>
+      <div className="navlinks">
+        <NavLink className="navlink" to="/projects">
+          Projects
+        </NavLink>
+        <NavLink to="/cv">CV</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+      </div>
+      <button
+        className="menuButton"
+        onClick={() => setMenuExpanded(!menuExpanded)}
+        aria-expanded={menuExpanded}
+        aria-label="Menu"
+      >
+        <MenuIcon />
+      </button>
+      {menuExpanded && <div>Expanded menu</div>}
     </nav>
   );
 }
