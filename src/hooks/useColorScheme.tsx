50e9056d3e40c3023ef0f423f2c59093a7e89f8d
Veera Alt <veera.alt@outlook.com>
2023-10-30 11:50:30 +0200

    Refactor color scheme hook

--- src/hooks/useColorScheme.tsx
+++ src/hooks/useColorScheme.tsx
@@ -1,41 +1,63 @@
 import { useEffect, useState } from "react";
 import { useMediaQuery } from "react-responsive";
 
+enum Theme {
+  LIGHT = "light",
+  DARK = "dark",
+}
+
+// Helper function to get theme from local storage
+const getStoredTheme = (): Theme | null => {
+  if (typeof window === "undefined") return null;
+  const theme = window.localStorage.getItem("data-theme");
+  return theme === Theme.LIGHT || theme === Theme.DARK ? theme : null;
+};
+
+// Helper function to store theme in local storage
+const storeTheme = (theme: Theme) => {
+  if (typeof window !== "undefined") {
+    window.localStorage.setItem("data-theme", theme);
+  }
+};
+
 /* Hook to decide whether light or dark mode should be used and to toggle between them.
  * It first checks if preference is saved to local storage (that is, if the user has visited
  * the website before with the same browser and used the dark mode switch. If no preference
  * was found that way, system preference is checked. If no preference was found from
  * system settings, light mode is used. */
 export const useColorScheme = () => {
-  /* Check system preference */
+  // Check system preference
   const systemPrefersDark = useMediaQuery({
     query: "(prefers-color-scheme: dark)",
   });
-  /* Ensure we're in a browser environment and check preference saved in local storage */
-  const darkPreferenceSaved =
-    typeof window !== "undefined"
-      ? window.localStorage.getItem("data-theme")
-      : null;
+
+  // Check local storage
+  const storedTheme = getStoredTheme();
 
   const [isDark, setIsDark] = useState<boolean | null>(
-    darkPreferenceSaved === null ? null : darkPreferenceSaved === "dark"
+    storedTheme ? storedTheme === Theme.DARK : null
   );
-  const value = isDark === null ? systemPrefersDark : isDark;
+  const currentTheme = isDark === null ? systemPrefersDark : isDark;
 
+  // Change the theme
   const handleDarkModeToggle = () => {
-    const newValue = !value;
-    if (typeof window !== "undefined")
-      window.localStorage.setItem("data-theme", newValue ? "dark" : "light");
-    setIsDark(!value);
+    const newTheme = !currentTheme;
+    storeTheme(newTheme ? Theme.DARK : Theme.LIGHT);
+    setIsDark(newTheme);
   };
 
+  /* Set 'data-theme' attribute to document body on the first render and
+   * each time the theme is changed */
   useEffect(() => {
     if (typeof document !== "undefined")
-      document.body.setAttribute("data-theme", value ? "dark" : "light");
-  }, [value]);
+      document.body.setAttribute(
+        "data-theme",
+        currentTheme ? Theme.DARK : Theme.LIGHT
+      );
+  }, [currentTheme]);
 
   return {
-    isDark: value,
+    isDark: currentTheme,
     handleDarkModeToggle,
   };
 };
