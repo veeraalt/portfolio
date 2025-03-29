da5af7fa9f5730abe594b67db520c64c9f91334c
Veera Alt <veera.alt@outlook.com>
2025-03-29 22:05:11 +1100

    Modernize design (#52)

--- src/views/landingView/LandingView.tsx
+++ src/views/landingView/LandingView.tsx
@@ -2,10 +2,9 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 import {
-  FaChevronRight as ArrowIcon,
   FaUser as PersonIcon,
-  FaBriefcase as WorkIcon,
-  FaPalette as HobbyIcon,
+  FaCode as WorkIcon,
+  FaBook as HobbyIcon,
 } from "react-icons/fa6";
 import "./LandingView.css";
 
@@ -14,19 +13,50 @@ const LandingView = () => {
 
   return (
     <div className="infoContainer">
-      <h1 className="shortIntro">{t("home.hi")}</h1>
-      <h2 className="name">{t("home.name")}.</h2>
-      <div className="introContainer">
-        <PersonIcon />
-        <p>{t("home.intro1")}</p>
-      </div>
-      <div className="introContainer">
-        <WorkIcon />
-        <p>{t("home.intro2")}</p>
-      </div>
-      <div className="introContainer">
-        <HobbyIcon />
-        <p>{t("home.intro3")}</p>
+      <h1 className="name">{t("home.name")}</h1>
+      <h2 className="shortIntro">{t("home.title")}</h2>
+      <div className="introOuterContainer">
+        <div className="introContainer">
+          <PersonIcon />
+          <p>{t("home.intro1")}</p>
+        </div>
+        <div className="introContainer">
+          <WorkIcon />
+          <p className="introText">
+            <span>{t("home.intro2")}</span>
+            <a
+              className="inlineLink highlight"
+              href="https://www.aalto.fi"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {t("home.keyword1")}
+            </a>
+            <span>{t("home.to")}</span>
+            <a
+              className="inlineLink highlight"
+              href="https://www.huutokaupat.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {t("home.keyword2")}
+            </a>
+            <span>{t("home.and")}</span>
+            <a
+              className="inlineLink highlight"
+              href="https://peoplecloudpro.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {t("home.keyword3")}
+            </a>
+            .
+          </p>
+        </div>
+        <div className="introContainer">
+          <HobbyIcon />
+          <p>{t("home.intro3")}</p>
+        </div>
       </div>
     </div>
   );
